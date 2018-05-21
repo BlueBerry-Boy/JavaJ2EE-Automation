28ca969b963d87161da67416c9c3e6b6fa31bdc0
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2018-05-20 20:47:07 -0500

    Automation Test

---
+++ Test_Automation/src/automation_frameworkNG/Arrays.java
@@ -0,0 +1,43 @@
+package automation_frameworkNG;
+
+	public class Arrays {
+
+		public static void main(String[] args) {
+			
+			// TODO arrays assignment;
+			
+			
+			
+//			int y [] = {3,6,9,10,11};
+//			
+//			for(int x=0; x<y.length; x++)
+//			{
+//				System.out.println(y[x]);
+//			}
+			
+			
+			
+			/**
+			 Multi Dimensional Array Syntax
+			 int a [] [] = new int [] [];
+			*/
+			
+			//Print the value on the 2nd row, 3rd column: (6) (Hint: index starts at 0)
+		
+			
+			int z [][] = 	{
+							 
+							 {1,2,3},
+							 {4,5,6},
+							 {7,8,9}
+							 
+							};
+							
+			System.out.println(z[1][2]);
+			
+		
+			
+			
+		}
+
+}
---
+++ Test_Automation/src/automation_frameworkNG/Loops_Do_while.java
@@ -0,0 +1,70 @@
+package automation_frameworkNG;
+
+	
+
+		public class Loops_Do_while {
+
+			public static void main(String[] args) {
+		
+		
+			//While loop
+
+			//1 to 10 
+			//int i=10;
+
+			/* while(i>0)
+			{
+			System.out.println(i);
+			i--;//i=2
+			}
+			}*/
+			
+			
+			/**
+				Do while loop 
+			*/
+			
+			int j=20; 
+			do
+			{
+			System.out.println(j);
+			j++;
+
+			} while(j>30);// 1 loop of execution is guarantee
+
+			
+			// For if loop
+
+			
+			// 1 to 10
+
+			/* for(initialization;condition;increment)
+			{
+
+			}*/
+			/* if(5>2)
+			{
+
+
+			System.out.println("success");
+			System.out.println("second step");
+			}
+
+			else
+
+			System.out.println("fail");*/
+
+
+			for(int x=0;x<10;x=x+3)
+			{
+				if(x==9)
+				System.out.println(" 9 is displayed");
+				else
+				System.out.println("I didnot find");//
+			}
+			
+
+			
+	}
+
+}
